9657de9e135d2019d462512281d626507d2d2889
occupy <occupy@ymail.com>
2011-10-15 23:09:03 -0700

    move inline js into application.js

--- javascripts/application.js
+++ javascripts/application.js
@@ -23,4 +23,12 @@ $(function(){
     $("#twitterText textarea").html('#' + hashtag + ' ');
     $("#twitterText textarea").keydown(updateTwitterLink);
     $("#twitterText a.twitter-share-button").click(post_user_message);
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=142371818162";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, "script", "facebook-jssdk"));
 });
